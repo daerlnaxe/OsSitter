26082dc7ef48b1c7682272c8e1750c5c0bfada33
CODOUL Alexandre <18460858+daerlnaxe@users.noreply.github.com>
2024-01-06 12:33:13 +0100

    Update DxHelios.py
    comments to help

--- DxHelios.py
+++ DxHelios.py
@@ -12,22 +12,33 @@ def get_sender(who):
     else:
         return who.__class__.__name__
 
+# Write like 'who {tab} | {tab} message'
 def Say (who, message: str, ind_class=0, ind_mess=0):   
     print(f"{get_sender(who)}"+"\t"*ind_class+" | " + "\t"*ind_mess + message)
 
+
+# Write like 'who {tab} | {tab} --Debug-- message'
 def Debug (who, message: str, ind_class=0, ind_mess=0):   
     print(f"{get_sender(who)}"+"\t"*ind_class+" | " + "\t"*ind_mess + "--Debug-- " + message)
 
+
+# write a title like #### who - bable ###
 def Title(who , title):
     mult=14
     print('#'*mult+f"{get_sender(who)}" + " - " + title +'#'*mult)
 
+
+# Draw a line
 def DrawLine():
     print('-'*200)
+
     
+# Write directly    
 def SayRaw(message):
     print(message)
 
+
+# Jump a line
 def Jump():
     print('')
     
