ce72166bdaea77f4cca342f4fec1ad2af04ed402
CODOUL Alexandre <18460858+daerlnaxe@users.noreply.github.com>
2024-01-07 14:22:00 +0100

    Update LinuxInstaller.sh

--- Resources/LinuxInstaller.sh
+++ Resources/LinuxInstaller.sh
@@ -3,14 +3,73 @@
 
 # Make me executable by using 'sudo chmod +x LinuxInstaller.sh'
 # Then run me by using './LinuxInstaller.sh'
-echo "### Welcome to OsSitter installer"
+echo "### Welcome to OsSitter installer for version 1.1"
+echo "Compatible OsSitter v4/4.1"
 echo "Note: The configuration file will not be overwritten"
+# stop if error
+set -e
+
+while getopts ":f:l:" opt; do
+  # Assign
+  case $opt in
+    f) tar_file="$OPTARG"
+    ;;
+    l) lang_file="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+  # Evaluate
+  #case $OPTARG in
+  #  -*) echo "Option $opt needs a valid argument"
+  #  exit 1
+  #  ;;
+  # esac
+done
+if [ -z "$tar_file" ] ; then
+        echo 'Missing -f' >&2
+        exit 1
+fi
+
+# Check tar file
+echo "Test '$tar_file'"
+if [[ ! -f $tar_file ]];then
+	echo "This file doesn't exists: $tar_file"
+	exit 10
+fi
+
+tar -tzf $tar_file > /dev/null 
+
+if [[ $? != 0 ]]
+then
+	echo "This tar file is invalid: $tar_file"
+	exit 11
+fi
+
+
+# Decompress
+if [[ ! -d ./tmp ]]
+then
+	mkdir ./tmp
+fi
+
+echo "Decompressing ..."
+# --Strip-components=1 remove root
+tar -xzf $tar_file -C tmp --strip-components=1
+
+
+
 read -p "Enter the installation folder: " installFolder
+if [[ -z "$installFolder" ]]; then
+    echo "Exit : no folder selected"
+	exit 1
+fi
 installFolder="$installFolder/OsSitter"
 echo "You want to install in '$installFolder'"
+fullIF=$(realpath $installFolder))
 
-#cd $installFolder || exit 1
-#cd -
+
 
 echo "language files"
 i=1
@@ -25,6 +84,7 @@ done
 read -p "Select a language: " languageRow
 languageFile=${arrVar[$languageRow]}
 
+
 if [[ -z "$languageFile" ]]; then
     echo "Exit : no language selected"
 	exit 1
@@ -34,25 +94,25 @@ echo "You selected the '$(basename $languageFile)' file"
 
 read -p "Press a key to continue"
 
-echo "Decompressing ..."
-mkdir ./tmp
-# --Strip-components=1 remove root
-tar -xzf OsSitter-Alpha4.tar.gz -C tmp --strip-components=1
 
-echo "Move files to installation folder"
 
-mkdir -p $installFolder
-cd $installFolder || exit 1
-cd -
+
+# Move files
+echo "Move files to installation folder $installFolder"
+mkdir -p $fullIF
+cd $fullIF || exit 1
+cd - > /dev/null
 
 for file in ./tmp/*.py;
 do
-	echo "moving ${file}";
-	mv ${file} $installFolder
+	echo "--- moving ${file}";
+	mv ${file} $fullIF
 done
 
-cp $languageFile $installFolder/currentlang.json
+cp $languageFile $fullIF/currentlang.json
 
+# Stop if error
+set -e
 echo "Modify rights on $installFolder recursively"
 chmod 744 $installFolder
 chmod 644 $installFolder/*
@@ -60,15 +120,17 @@ echo "Giving execution to $installFolder/OsSitter.py"
 chmod 744 $installFolder/OsSitter.py
 
 echo "Modify ossitter.service"
-pyth_file=$(realpath $installFolder/OsSitter.py)
-echo $pyth_file
+#pyth_file=$(realpath $installFolder/OsSitter.py)
+#echo $pyth_file
 # PrincipeExecStart=/bin/bash -c 'cd /home/ubuntu/project/ && python app.py'
 sed -i "s,{InstallFolder},$installFolder,gm" "./tmp/Resources/ossitter.service"
 
 
 
 echo "Changing owner to root"
-sudo chown root: $installFolder
+
+
+sudo chown root: $fullIF
 
 
 # install as a service
